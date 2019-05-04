0dad799e9e586b47477d435816c03a621545ae3b
Kambiz Guity <kg@pazhg.com>
2019-05-04 13:03:28 +0430

    Initial Commit

---
+++ PazhgContact/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PazhgContact
+//
+//  Created by Pazhg on 2/14/1398 AP.
+//  Copyright © 1398 Pazhg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
